b21dc57ceb10a3eb2d3d345f130fc571daf1df8b
Kucheryavenkov <igor.kucheryavenkov@autofaq.ai>
2026-05-08 17:37:54 +0300

    trying DOM

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,15 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     dom.console.log("wtf")
+    import scala.scalajs.js
+    import scala.scalajs.js.timers._
+
+    println("Начало")
+
+    // setTimeout на 1000 миллисекунд
+    js.timers.setTimeout(1000) {
+      println("Прошла 1 секунда")
+    }
   }
   println("rld!")
 }
