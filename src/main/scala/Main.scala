ea6866856f04a86779c6e17531a3b5ee4c55581c
Kucheryavenkov <igor.kucheryavenkov@autofaq.ai>
2026-05-08 16:49:14 +0300

    trying DOM

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,13 @@
+import org.scalajs.dom
+
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("app")
 object Main {
-  def main(args: Array[String]): Unit = {
-    import org.scalajs.dom
-    dom.window.alert("Hi from Scala-js-dom")
-    println("Hello world!")
+  import org.scalajs.dom._
+  def main(div: html.Div): Unit = {
+    val child = dom.document.createElement("div")
+    child.textContent = "Hi from Scala-js-dom"
+    div.appendChild(child)
   }
 }
