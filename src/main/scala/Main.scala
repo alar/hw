3ccbc39c9a5b892c6044eca7586592390d3dc7b4
Kucheryavenkov <igor.kucheryavenkov@autofaq.ai>
2026-05-08 17:31:19 +0300

    trying DOM

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,12 @@
+import org.scalajs.dom
+
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("app")
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    dom.console.log("wtf")
   }
   println("rld!")
 }
